6633a386c6f6e221a2f7b9e0e043cfc53c9fb882
zaidanav <13522146@std.stei.itb.ac.id>
2024-10-15 08:11:42 +0700

    buat uji coba postgresql

--- php/src/index.php
+++ php/src/index.php
@@ -1,4 +1,19 @@
 <?php
+$host = 'db'; // Nama service database di docker-compose.yml
+$dbname = 'linkinpurry_db';
+$user = 'user';
+$password = 'userpassword';
 
-echo 'Hello World!';
-echo '<script src="/public/index.js"></script>';
+$dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$user;password=$password";
+try {
+    // Create a PostgreSQL database connection
+    $conn = new PDO($dsn);
+    
+    // Display a message if connected successfully
+    if($conn) {
+        echo "Connected to the PostgreSQL database successfully!";
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
