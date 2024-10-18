7c0191e8a186ca5fcf996e1b6a5d899b3fef2eac
Aland Mulia Pratama <13522124@std.stei.itb.ac.id>
2024-10-19 00:51:09 +0700

    feat: add applicant details module

--- php/src/edit_lowongan.php
+++ php/src/edit_lowongan.php
@@ -47,7 +47,7 @@ if (!$lowongan) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Buat Lowongan Baru</title>
+    <title>Edit Lowongan</title>
     <link rel="stylesheet" href="css/styles_js.css"> <!-- Menggunakan CSS global -->
 </head>
 <body>
