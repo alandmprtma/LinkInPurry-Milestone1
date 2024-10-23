b55b06a13ed712844a4b377fe0c28aff6ede44a8
Aland Mulia Pratama <13522124@std.stei.itb.ac.id>
2024-10-23 22:53:05 +0700

    refactor: script detail-lowongan

--- php/src/edit_lowongan.php
+++ php/src/edit_lowongan.php
@@ -259,10 +259,5 @@ if (!$lowongan) {
 
 
 </body>
+<script src=public/hamburger></script>
 </html>
-<script>
-    document.getElementById('hamburger-menu').addEventListener('click', function() {
-        const navLinks = document.getElementById('nav-links'); // Pastikan ID-nya sesuai
-        navLinks.classList.toggle('active');
-    });
-</script>
