fdcc9061c213a07f026740f32fc0c3aba9b8a226
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2024-10-24 16:48:57 +0700

    feat : profile edit functionalities

--- php/src/profile.php
+++ php/src/profile.php
@@ -20,36 +20,81 @@ try {
     die("Koneksi ke database gagal: " . $e->getMessage());
 }
 
-// Toss the mf user back if he inputted nothing lmao
-// Cek apakah pengguna sudah login
-if (!isset($_GET['user_id'])) {
-    if ($_SESSION['role'] == "company") {
-        header('Location: home_company.php');
+// handle PUT request
+if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    //only authenticate if the same user
+    //otherwise fuck off
+    $updatedata = file_get_contents("php://input");
+    $data = json_decode($updatedata, true);
+    $query = "";
+    $vars = [
+        'activeuser' => $_SESSION['user_id']
+    ];
+    $retval = array("success"=>FALSE,"reason"=>"");
+
+    if (isset($data["location"])){
+        $query .= "UPDATE companydetail SET lokasi = :lokasi
+                   WHERE user_id = :activeuser";
+        $vars['lokasi'] = $data["location"];
     }
-    else if ($_SESSION['role'] == "jobseeker") {
-        header('Location: home_jobseeker.php');
+    else if (isset($data["about"])){
+        $query .= "UPDATE companydetail SET about = :about
+                   WHERE user_id = :activeuser";
+        $vars['about'] = $data["about"];
     }
-    else {
-        header('Location: auth/login.html');
+    else{
+        $retval["reason"] = "Invalid PUT parameters";
+        echo $retval;
+        exit();
     }
+
+    $stmt = $pdo->prepare($query);
+    $stmt->execute($vars);
+    $rowcount = $stmt->rowCount();
+
+    if ($rowcount > 0){
+        $retval['success'] = TRUE;
+    }
+
+    echo json_encode($retval);
     exit();
 }
+else if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+    // Toss the mf user back if he inputted nothing lmao
+    // Cek apakah pengguna sudah login
 
-// Get profile id
-$wanted_id = $_GET['user_id'];
+    if (!isset($_GET['user_id'])) {
+        if ($_SESSION['role'] == "company") {
+            header('Location: home_company.php');
+        }
+        else if ($_SESSION['role'] == "jobseeker") {
+            header('Location: home_jobseeker.php');
+        }
+        else {
+            header('Location: auth/login.html');
+        }
+        exit();
+    }
+    // Get profile id
+    $wanted_id = $_GET['user_id'];
 
-// Query untuk mendapatkan detail lamaran
-$query ="SELECT u.nama as nama, c.*
-         FROM users AS u, companydetail AS c
-         WHERE u.user_id = :wanted_user AND u.user_id = c.user_id";
-$stmt = $pdo->prepare($query);
-$stmt->execute(['wanted_user' => $wanted_id]);
-$profile = $stmt->fetch();
+    // Query untuk mendapatkan detail lamaran
+    $query ="SELECT u.nama as nama, c.*
+            FROM users AS u, companydetail AS c
+            WHERE u.user_id = :wanted_user AND u.user_id = c.user_id";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute(['wanted_user' => $wanted_id]);
+    $profile = $stmt->fetch();
 
-if (!$profile) {
-    echo "<p>Profile perusahaan tidak ditemukan.</p>";
-    exit();
+    if (!$profile) {
+        echo "<p>Profile perusahaan tidak ditemukan.</p>";
+        exit();
+    }
+
+    
+    //$success = isset($_GET["success"]) ? '<div class="yipee"><h2>Update success!</h2></div>' : '';
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -59,27 +104,78 @@ if (!$profile) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile Company</title>
     <link rel="stylesheet" href="../css/styles.css"> <!-- Menggunakan CSS global -->
+    <link rel="stylesheet" href="css/styles_js.css">
     <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css">
+    <link rel="stylesheet" href="../css/styles_p.css">
+    <link href="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.snow.css" rel="stylesheet" />
+    
 </head>
-<body style="flex-direction: column; justify-content: flex-start; width: 100%; background-color: #EFEFEF;">
-    <div class="box" style=" width: 80% ;border: 10%; border-radius: 1dvw; margin: 1%; background-color: white; box-shadow: 0px 0px 5px;">
-        <div class="nameplate" style="background-color: #D9D9D9; width: 80%; padding: 5% 10% 5% 10%; border-top-left-radius: inherit; border-top-right-radius: inherit;">
+<body>
+    <nav class="navbar">
+        <img class="logo" src="assets/LinkInPurry-crop.png" alt=".">
+        <form method="GET" action="home_company.php" id="search-form">
+        <div class="search-bar">
+            <div class="icon">
+                <img src="assets\search-icon-removebg-preview-mirror.png" alt="Search Icon">
+            </div>
+            <div class="search-bar-container">
+            <input type="hidden" id="company_id" name="company_id" value="<?php echo $_SESSION['user_id'];?>">
+            <input type="text" id="search_keyword"  name="search_keyword" onkeyup="handleSearchInput(event)" placeholder="Search by position or company" value="<?= isset($_GET['search_keyword']) ? htmlspecialchars($_GET['search_keyword']) : '' ?>">
+            <div id="autocomplete-results" class="autocomplete-results"></div>
+            </div>
+        </div>
+        </form>
+        <div class="hamburger-menu" id="hamburger-menu">
+            <i class="fas fa-bars"></i>
+        </div>
+        <ul class="nav-links" id="nav-links">
+            <li><a class="current" href="/"> <img src="assets/home_black.png" alt="."> Home</a></li>
+            <li><a class="inactive" href="auth/logout.php"> <img class="logout" src="assets/logout-grey.png" alt="."> Log Out</a></li>
+        </ul>
+    </nav> 
+    <div class="box shadowed">
+        <div class="nameplate">
         <!-- <img style="width: 10dvw; height: 10dvw; transform: translateY(90%);" src="../test.png"> -->
         </div>
         <div class="text-content" style="padding: 5%;">
-            <h1><?php echo $profile["nama"]?></h1>
-            <i class="fa-solid fa-location-dot"></i> <?php echo $profile["lokasi"]?>
+            <h1 id="nama"><?php echo $profile["nama"]?></h1>
+            <span class="location-span">
+                <i class="fa-solid fa-location-dot"></i>
+                <div id="location"><?php echo $profile["lokasi"]?></div>
+                <div id="location-edit" hidden>
+                    <input type="text" id="location-edit-box">
+                </div>
+                <i class="fa-solid fa-pencil fa-sm" onclick="EditLocationToggle()"></i>
+            </span>
+            
+
         </div>
     </div>
-    <div class="box" style="width: 80% ;border: 10%; border-radius: 1dvw; margin: 1%; background-color: white; box-shadow: 0px 0px 5px;">
+    <div class="box shadowed">
         <div class="text-content" style="padding: 5%;">
-            <h1>About</h1>
-            <p><?php echo $profile["about"]?></p>
+            <h1>About <i onclick="EditAboutToggle()" class="fa-solid fa-pencil fa-sm"></i></h1>
+            <div id="about"><?php echo $profile["about"]?></div>
+            <div id="about-edit" hidden>
+                <!-- editor -->
+                <div id="editor" style="height: 200px;"><?php echo $profile["about"]?></div>
+                <!-- Include the Quill library -->
+                <script src="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.js"></script>
+                <!-- Initialize Quill editor -->
+                <script>
+                    const quill = new Quill('#editor', {
+                    theme: 'snow'
+                    });
+                </script>
+                <span class="profile_editor_div">
+                    <button onclick="EditAboutToggle()" id="profile_editor_button about-save" class="">Cancel</button>
+                    <button onclick="updateInfo('about')" id="profile_editor_button about-save" class="">Save</button>
+                </span>
+            </div>
         </div>
-
     </div>
     <div class="deadspace" style="height: 5%;">
         <hr>
     </div>
 </body>
+<script src="public/profile.js"></script>
 </html>
